292a8f88e9d63f8ecf5f3e436569f52fc2049f61
Joshua Mitchell <joshua@gpsimpact.com>
2017-10-25 15:36:26 -0500

    init ch-03

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, jsonify
-import boto3
-
+from flask import Flask, jsonify, request
 app = Flask(__name__)
+
 
 
 @app.route('/')
@@ -16,14 +15,35 @@ session = boto3.Session()
 dynDB = session.resource('dynamodb')
 table = dynDB.Table('PYKC-Auth')
 
-#I don't actually recommend creating this endpoint. just for demo purposes.
-@app.route('/<email>')
-def check_email(email):
-    response = table.get_item(Key={"email": email.upper()})
-    if response.get('Item', None):
-        return jsonify(response)
-    else:
-        return jsonify({'error': 'user not found'})
+
+import bcrypt
+import time
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    # REQUIRE AN EMAIL AND PASSWORD
+    if not data.get('email', None) or not data.get('password', False):
+        return jsonify({'error': 'must provide email and password'})
+
+    # VERIFY THAT THIS USER DOES NOT ALREADY EXIST
+    check_status = table.get_item(Key={"email": str(data['email']).upper()})
+    if(check_status.get('Item')):
+        return jsonify({'error': 'profile exists already'})
+
+
+    #CREATE A HASHED PASSWORD AND POST OBJECT
+    hashed_password = bcrypt.hashpw(str(data['password']), bcrypt.gensalt())
+    post_obj = {
+        'email': data['email'].upper(),
+        'password_hash': hashed_password,
+        'created_at': int(time.time())
+    }
+
+    save_user = table.put_item(
+        Item=post_obj
+    )
+
+    return jsonify({'message': 'user created'})
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ cmds/test_register.sh
@@ -0,0 +1,1 @@
+http POST 127.0.0.1:5000/register email=newuser@foo.com password=pykc!
